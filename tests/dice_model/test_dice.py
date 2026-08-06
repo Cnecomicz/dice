64d7a072c96782088bbe3bdccef96d11e318dd28
Eric <eric@locallyringed.space>
2026-08-06 18:09:00 -0500

    Red: test_parsing_dice_string_syntax

--- tests/dice_model/test_dice.py
+++ tests/dice_model/test_dice.py
@@ -22,3 +22,13 @@ def test_dice_validation():
         Dice(quantity=2, sides=6, keep=0)
     with raises(ValueError):
         Dice(sides=6, multiplier=0)
+
+# You can generate Dice from string syntax
+def test_parsing_dice_string_syntax():
+    assert parse("d20") == Dice(sides=20)
+    assert parse("3d6") == Dice(quantity=3, sides=6)
+    assert parse("3d20k1") == Dice(quantity=3, sides=20, keep=1)
+    assert parse("d4+2") == Dice(sides=4, summand=2)
+    assert parse("3d6*10") == Dice(quantity=3, sides=6, multiplier=10)
+    assert parse("l*d6") == Dice(sides=6, per_level=True)
+    assert parse("l*2d6k1*3+2") == Dice(quantity=2, sides=6, keep=1, multiplier=3, summand=2, per_level=True)
