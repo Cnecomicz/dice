93f04e46a5a9fbe72af4062e2c94e935c2b89099
Eric <eric@locallyringed.space>
2026-08-06 17:26:46 -0500

    Red: test_dice_creation

---
+++ tests/dice_model/test_dice.py
@@ -0,0 +1,9 @@
+from dice_model.dice import Dice
+
+# Dice can be created. NB: I'm creating examples straight from the TTRPG
+# which demonstrate the need for these attrs
+def test_dice_creation():
+    extreme_character_creation = Dice(quantity=3, sides=20, keep=1)
+    cleric_spells_gained = Dice(sides=4, summand=2)
+    starting_gold = Dice(quantity=3, sides=6, multipler=10)
+    elemental_blast_spell_damage = Dice(sides=6, per_level=True)
