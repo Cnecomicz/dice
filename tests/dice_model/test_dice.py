ae57b8c2f6d83a63fd23645dcc57ecd7c0502999
Eric <eric@locallyringed.space>
2026-08-06 17:46:56 -0500

    Red: test_dice_validation

--- tests/dice_model/test_dice.py
+++ tests/dice_model/test_dice.py
@@ -1,3 +1,5 @@
+from pytest import raises
+
 from dice_model.dice import Dice
 
 # Dice can be created. NB: I'm creating examples straight from the TTRPG
@@ -7,3 +9,16 @@ def test_dice_creation():
     cleric_spells_gained = Dice(sides=4, summand=2)
     starting_gold = Dice(quantity=3, sides=6, multiplier=10)
     elemental_blast_spell_damage = Dice(sides=6, per_level=True)
+
+# Invalid Dice raise errors
+def test_dice_validation():
+    with raises(ValueError):
+        Dice(quantity=0, sides=6)
+    with raises(ValueError):
+        Dice(sides=0)
+    with raises(ValueError):
+        Dice(quantity=2, sides=6, keep=3)
+    with raises(ValueError):
+        Dice(quantity=2, sides=6, keep=0)
+    with raises(ValueError):
+        Dice(sides=6, multiplier=0)
