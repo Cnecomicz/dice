ac7d543e74a310758279e738d552063fe5e7a3a0
Eric <eric@locallyringed.space>
2026-08-06 19:03:28 -0500

    Red: test_usagedie_creation

---
+++ tests/dice_model/test_usage_die.py
@@ -0,0 +1,7 @@
+from dice_model.usage_die import UsageDie
+
+# UsageDie can be created.
+def test_usagedie_creation():
+    arrows = UsageDie(10, 8, 6, 4)
+    level_10_magic_user_spells_per_day = UsageDie(10, 8, 6, 4, 20, 12, 10, 8, 6, 4)
+    level_10_warlock_spells_per_day = UsageDie(20, 16, 14, 12, 10, 8, 7, 6, 5, 4)
