014b8a45d715a736feab43fdc8230e8be6eebe05
Eric <eric@locallyringed.space>
2026-08-07 18:47:03 -0500

    Red: test_roll_arithmetic

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,14 @@
-from typing import Callable
-
 from pytest import fixture
 
+
 class MockRng:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, *values):
+        self.values = iter(values)
 
     def randint(self, low, high):
-        return self.value
+        return next(self.values)
 
 
 @fixture
 def mock_rng():
-    return lambda value: MockRng(value)
+    return MockRng
--- tests/dice_roller/test_roll.py
+++ tests/dice_roller/test_roll.py
@@ -24,3 +24,25 @@ def test_face_alias(mock_rng):
 def test_result_converts_to_total(mock_rng):
     result = roll(Dice(quantity=3, sides=10, keep=2))
     assert int(result) == result.total
+
+# All the roll operations work: you sum, keep, multiply, add, level scale
+def test_roll_arithmetic(mock_rng):
+    result_1 = roll(Dice(quantity=3, sides=6), rng=mock_rng(1, 5, 3))
+    assert result_1.faces == (1, 5, 3)
+    assert result_1.kept == (1, 5, 3)
+    assert result_1.total == 9
+    result_2 = roll(Dice(quantity=3, sides=20, keep=1), rng=mock_rng(7, 19, 12))
+    assert result_2.faces == (7, 19, 12)
+    assert result_2 .kept == (19,)
+    assert result_2.total == 19
+    result_3 = roll(Dice(quantity=3, sides=6, multiplier=10), rng=mock_rng(2, 4, 6))
+    assert result_3.total == 120
+    result_4 = roll(Dice(quantity=2, sides=8, summand=1), rng=mock_rng(3, 5))
+    assert result_4.total == 9
+    result_5 = roll(Dice(sides=6, multiplier=10, summand=1), rng=mock_rng(2))
+    assert result_5.total == 21
+    result_6 = roll(Dice(sides=6, per_level=True), level=3, rng=mock_rng(1, 2, 3))
+    assert result_6.faces == (1, 2, 3)
+    assert result_6.total == 6
+    result_7 = roll(Dice(quantity=2, sides=6), level=5, rng=mock_rng(4, 4))
+    assert result_7.faces == (4, 4)
