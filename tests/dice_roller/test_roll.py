94d3a38bd487a58c38052fd645e07f8648d467f4
Eric <eric@locallyringed.space>
2026-08-07 18:22:55 -0500

    Green: test_face_alias

--- tests/dice_roller/test_roll.py
+++ tests/dice_roller/test_roll.py
@@ -1,3 +1,5 @@
+from pytest import raises
+
 from dice_model.dice import Dice
 from dice_roller.roll import RollResult, roll
 
@@ -8,3 +10,12 @@ def test_rolling_a_die(mock_rng):
     assert result.faces == (4,)
     assert result.kept == (4,)
     assert result.total == 4
+
+# face is only valid for single die rolls and can be distinct from total
+def test_face_alias(mock_rng):
+    result_1 = roll(Dice(quantity=3, sides=6))
+    with raises(ValueError):
+        result_1.face
+    result_2 = roll(Dice(sides=6, multiplier=10, summand=1), rng=mock_rng(4))
+    assert result_2.face == 4
+    assert result_2.total == 41
