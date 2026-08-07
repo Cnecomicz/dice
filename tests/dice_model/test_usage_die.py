51a079edeae5896e772800d67ec7acce2ca6b79f
Eric <eric@locallyringed.space>
2026-08-07 15:40:25 -0500

    Red: test_usagedie_use

--- tests/dice_model/test_usage_die.py
+++ tests/dice_model/test_usage_die.py
@@ -2,7 +2,7 @@ from pytest import raises
 
 from dice_model.usage_die import UsageDie
 
-# UsageDie can be created.
+# UsageDie can be created
 def test_usagedie_creation():
     arrows = UsageDie(10, 8, 6, 4)
     level_10_magic_user_spells_per_day = UsageDie(10, 8, 6, 4, 20, 12, 10, 8, 6, 4)
@@ -12,3 +12,17 @@ def test_usagedie_creation():
 def test_usagedie_validation():
     with raises(ValueError):
         UsageDie()
+
+# UsageDie is used and returns a UsageResult
+def test_usagedie_use():
+    die = UsageDie(6, 4)
+    result = die.use()
+    assert result.face in {1, 2, 3, 4, 5, 6}
+    assert result.sides_before == 6
+    assert result.sides_after == 4 if result.face in {1, 2} else result.sides_after == 6
+    assert result.downgraded if result.sides_after == 4 else not result.downgraded
+    assert not result.exhausted
+    assert die.position == 0 if result.sides_after == 6 else die.position == 1
+
+
+
