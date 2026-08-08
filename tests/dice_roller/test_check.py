ddd33af7eb5873e98d3aacda34adf91a890f1114
Eric <eric@locallyringed.space>
2026-08-07 19:57:23 -0500

    Refactor: made tests more readable

--- tests/dice_roller/test_check.py
+++ tests/dice_roller/test_check.py
@@ -2,22 +2,23 @@ from dice_roller.check import thread_the_needle
 
 # You can thread the needle
 def test_thread_the_needle(mock_rng):
-    for i in range(1, 21):
-        result = thread_the_needle(above=5, below=15, rng=mock_rng(i))
-        assert result.rolls == (i,)
+    for roll in range(1, 21):
+        result = thread_the_needle(above=5, below=15, rng=mock_rng(roll))
+        assert result.rolls == (roll,)
         assert result.lower_bound == 5
         assert result.upper_bound == 15
         assert result.advantage == 0
-        assert result.success if i in {6, 7, 8, 9, 10, 11, 12, 13, 14} else not result.success
-    for j in range(-2, 3):
-        all_rolls = [8, 10, 12]
-        attempt = all_rolls[:abs(j)+1]
-        result = thread_the_needle(above=8, below=12, advantage=j, rng=mock_rng(*attempt))
-        assert result.advantage == j
-        assert (
-            result.success 
-            if j >= 0 and any(roll in {9, 10, 11} for roll in attempt)
-            or j <0 and all(roll in {9, 10, 11} for roll in attempt)
-            else not result.success
-        )
+        assert result.success if roll in {6, 7, 8, 9, 10, 11, 12, 13, 14} else not result.success
+    for advantage in range(0, 3):
+        advantage_rolls = [8, 9, 10]
+        advantage_attempt = advantage_rolls[:advantage+1]
+        advantage_result = thread_the_needle(above=8, below=12, advantage=advantage, rng=mock_rng(*advantage_attempt))
+        assert advantage_result.advantage == advantage
+        assert advantage_result.success if advantage in {1, 2} else not advantage_result.success
+    for disadvantage in range(-2, 1):
+        disadvantage_rolls = [10, 9, 8]
+        disadvantage_attempt = disadvantage_rolls[:-disadvantage+1]
+        disadvantage_result = thread_the_needle(above=8, below=12, advantage=disadvantage, rng=mock_rng(*disadvantage_attempt))
+        assert disadvantage_result.advantage == disadvantage
+        assert disadvantage_result.success if disadvantage in {-1, 0} else not disadvantage_result.success
 
